1d556c9471dfb0d778650b22f9a681fd5ade0963
Quinn Ngo <quinn.ngo@joinhoney.com>
2019-09-29 01:42:29 -0700

    Add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import helmet from 'helmet';
+import logger from 'morgan';
 import webhookRouter from './routers/webhooks';
 
 const app = express();
+app.use(helmet());
+app.use(logger('dev'))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 app.use('/webhooks', webhookRouter);
 
 export default app;
