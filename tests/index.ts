8c743d19ff9424e3e20f5a2ede246b14dedfe699
Quinn Ngo <quinn.ngo@joinhoney.com>
2019-09-29 10:55:33 -0700

    Add ava tests

---
+++ tests/index.ts
@@ -0,0 +1,6 @@
+import test from 'ava';
+const fn = () => 'foo';
+
+test('fn() returns foo', t=> {
+  t.is(fn(), 'foo');
+});
---
+++ tests/something_else/hello.ts
@@ -0,0 +1,5 @@
+import test from 'ava';
+
+test('hello', t => {
+    t.true(false);
+});
