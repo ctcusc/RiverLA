06c6c533042cec6dbd7a9c12ee90859f062dc858
Quinn Ngo <quinn.ngo@joinhoney.com>
2019-10-23 02:38:58 -0700

    Add skeleton tests

---
+++ tests/apiClients/AirtableApiClient.ts
@@ -0,0 +1,5 @@
+import test from 'ava';
+
+test('Returns one page of results properly', t => {
+  t.true(false, 'replace this test');
+});
--- tests/something_else/hello.ts
+++
@@ -1,5 +0,0 @@
-import test from 'ava';
-
-test('hello', t => {
-  t.true(true);
-});
