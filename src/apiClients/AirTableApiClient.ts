ddd477480aa908e72cc544f165231e03b711a713
madelyndubuk <dubuk@usc.edu>
2019-10-27 12:27:40 -0700

    get all organizations no filter

--- src/apiClients/AirTableApiClient.ts
+++ src/apiClients/AirTableApiClient.ts
@@ -1,20 +1,31 @@
+import Airtable from 'airtable';
+
 interface AirTableFilters {
   interestCategories?: string[];
   riverSections?: string[];
 }
 
-interface Organization {
+export interface Organization {
   name: string;
   // fill in the rest
+  description: string;
+  activity: string;
+  interestCategories: string[];
+  riverSections: string;
+  phoneNumber: string;
+  url: string;
+  email: string;
 }
 
 class AirTableApiClient {
   private apiKey: string;
+  private base: any;
 
   constructor(apiKey: string) {
     this.apiKey = apiKey;
+    this.base = new Airtable({ apiKey: apiKey }).base('appEHr8iHguvEfXTQ');
     // remove this log when this API key is finally used somewhere
-    console.log(`SendGridAPIClient initialized with api key ${this.apiKey}`);
+    console.log(`AirTableAPIClient initialized with api key ${this.apiKey}`);
   }
 
   async getAllOrganizations(filters: AirTableFilters = {}): Promise<Organization[]> {
@@ -24,9 +35,45 @@ class AirTableApiClient {
       `interestCategories: ${interestCategories}`,
       `riverSections: ${riverSections}`,
     );
+    const organizations: Organization[] = [];
+
+    const promise: Promise<Organization[]> = new Promise((resolve, reject) => {
+      this.base('Organizations')
+        .select({
+          view: 'Grid view',
+        })
+        .eachPage((records: any[], fetchNextPage: any) => {
+          organizations.concat(
+            records.filter().map((record: any) => {
+              if (typeof record === 'undefined') {
+              }
+              const org: Organization = {
+                name: record.get('Name'),
+                description: record.get('Description'),
+                activity: record.get('Activity'),
+                interestCategories: record.get('Interest Categories'),
+                riverSections: record.get('River Section'),
+                phoneNumber: record.get('Phone Number'),
+                url: record.get('URL'),
+                email: record.get('Email'),
+              };
+              console.log(org.name);
+              return org;
+            }),
+          );
+          fetchNextPage();
+        }),
+        function done(err: any) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(organizations);
+          }
+        };
+    });
     // returns a promise that resolves with all organizations that correspond to passed in interestCategories and riverSections.
     // if interestCategories/riverSections is not passed in, we do not need to filter based on those things
-    return [];
+    return promise;
   }
 }
 
