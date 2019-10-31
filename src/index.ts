3e0d64bbc0964800560bee4f5af9402891015c0c
Daniel Koo <dkoo.dev@gmail.com>
2019-10-30 19:14:12 -0700

    nation builder wip
    Added npm package ngrok
    Added to webhbooks router
    Changed dev port to 80 (not confident)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// Import modules
+import ngrok = require('ngrok');
+
 import apiClients from './apiClients';
 import app from './app';
 import env from './env';
@@ -7,3 +10,12 @@ app.listen(env.server.port, () => {
   console.log(apiClients.sendgridApiClient);
   console.log(`🚀 App listening on port ${env.server.port}!`);
 });
+
+(async function() {
+  const ngrokUrl = await ngrok.connect();
+  console.log(
+    `[Instructions to test webhook]
+    Go to https://larivercorp.nationbuilder.com/admin/webhooks/new and
+    add a new webhook with Endpoint URL: ${ngrokUrl}.`,
+  );
+})();
--- src/routers/webhooks.ts
+++ src/routers/webhooks.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-router.use('/', (_, res) => {
-  res.send('webhook route');
+router.use('/', (_, res, next) => {
+  next();
+});
+
+router.post('/nationbuilder/personCreated', function(req, res) {
+  res.send('Got a POST request');
 });
 
 export default router;
