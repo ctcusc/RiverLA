e79401f15bd3591d231b01fa25a24088f83d8b3e
madelyndubuk <dubuk@usc.edu>
2019-11-08 10:09:16 -0800

    wrote tests

--- test/apiClients/AirtableApiClient.ts
+++ test/apiClients/AirtableApiClient.ts
@@ -1,55 +1,55 @@
+import AirTableApiClient, { AirTableFilters, Organization } from '../../src/apiClients/AirTableApiClient';
+import nock from 'nock';
 import test from 'ava';
-import sinon from 'sinon';
-import AirTableApiClient, { Organization } from '../../src/apiClients/AirTableApiClient';
-//import env from '../../src/env';
 
-test('Returns one page of results properly', async t => {
-  const airtableApiClient: AirTableApiClient = new AirTableApiClient('keyzxy');
-  const eachPageStub = sinon.stub().returns(records);
-  sinon.stub(airtableApiClient.base, 'select').returns({ eachPage: eachPageStub });
-  const organizations: Organization[] = await airtableApiClient.getAllOrganizations();
-  //const getOrganizationsStub = sinon.stub(airtableApiClient, 'getAllOrganizations').callsFake(lastStub);
-  //getOrganizationsStub();
-  const answer: Organization[] = [org1, org2];
-  t.deepEqual(organizations, answer);
-});
-
-const records: Object = {
-  records: [
-    {
-      id: 'abc123',
-      fields: {
-        Name: 'Chrysalis Enterprises',
-        'River Section': 'Upper',
-        Activity: 'Needs Volunteers',
-        Description:
-          'Chrysalis is a nonprofit organization dedicated to creating a pathway to self-sufficiency for homeless and low-income individuals by providing the res...',
-        Email: 'fakeemail@chrysalis.org',
-        'Phone Number': '(123) 456-7890',
-        URL: 'https://changelives.org/',
-        'Interest Categories': ['Water Organizations'],
-      },
-      createdTime: '2019-10-15T18:32:03.000Z',
+const records: any = [
+  {
+    id: 'abc123',
+    fields: {
+      Name: 'Chrysalis Enterprises',
+      'River Section': 'Upper',
+      Activity: 'Needs Volunteers',
+      Description:
+        'Chrysalis is a nonprofit organization dedicated to creating a pathway to self-sufficiency for homeless and low-income individuals by providing the res...',
+      Email: 'fakeemail@chrysalis.org',
+      'Phone Number': '(123) 456-7890',
+      URL: 'https://changelives.org/',
+      'Interest Categories': ['Water Organizations'],
     },
-    {
-      id: 'def456',
-      fields: {
-        Name: 'Arroyo Seco Foundation',
-        'River Section': 'Middle',
-        Activity: 'Needs Volunteers',
-        Description:
-          'Volunteers are contributing in a big way to restoring the beauty and splendor of the Arroyo Seco, and you can make a difference too. Volunteers partic...',
-        Email: 'info@arroyoseco.org',
-        'Phone Number': '(098) 765-4321',
-        URL: 'https://www.arroyoseco.org/getinvolved.htm',
-        'Interest Categories': ['Water Organizations', 'Environmental Causes'],
-      },
-      createdTime: '2019-10-18T22:54:50.000Z',
+    createdTime: '2019-10-15T18:32:03.000Z',
+  },
+  {
+    id: 'def456',
+    fields: {
+      Name: 'Arroyo Seco Foundation',
+      'River Section': 'Middle',
+      Activity: 'Needs Volunteers',
+      Description:
+        'Volunteers are contributing in a big way to restoring the beauty and splendor of the Arroyo Seco, and you can make a difference too. Volunteers partic...',
+      Email: 'info@arroyoseco.org',
+      'Phone Number': '(098) 765-4321',
+      URL: 'https://www.arroyoseco.org/getinvolved.htm',
+      'Interest Categories': ['Water Organizations', 'Environmental Causes'],
     },
-  ],
-};
+    createdTime: '2019-10-18T22:54:50.000Z',
+  },
+  {
+    id: 'ghi789',
+    fields: {
+      Name: 'ctc',
+      'River Section': 'Lower',
+      Activity: 'Needs Volunteers',
+      Description: 'test data',
+      Email: 'fakeemail@chrysalis.org',
+      'Phone Number': '(999) 999-9999',
+      URL: 'https://changelives.org/',
+      'Interest Categories': ['Social Justice and Recreation'],
+    },
+    createdTime: '2019-10-18T22:54:50.000Z',
+  },
+];
 
-const org1: Organization = {
+const org2: Organization = {
   name: 'Arroyo Seco Foundation',
   riverSection: 'Middle',
   activity: 'Needs Volunteers',
@@ -61,7 +61,7 @@ const org1: Organization = {
   interestCategories: ['Water Organizations', 'Environmental Causes'],
 };
 
-const org2: Organization = {
+const org1: Organization = {
   name: 'Chrysalis Enterprises',
   riverSection: 'Upper',
   activity: 'Needs Volunteers',
@@ -72,3 +72,152 @@ const org2: Organization = {
   url: 'https://changelives.org/',
   interestCategories: ['Water Organizations'],
 };
+
+const org3: Organization = {
+  name: 'ctc',
+  riverSection: 'Lower',
+  activity: 'Needs Volunteers',
+  description: 'test data',
+  email: 'fakeemail@chrysalis.org',
+  phoneNumber: '(999) 999-9999',
+  url: 'https://changelives.org/',
+  interestCategories: ['Social Justice and Recreation'],
+};
+
+test('Returns one page of results properly', async t => {
+  const airtableApiClient: AirTableApiClient = new AirTableApiClient('keyzxy');
+
+  nock('https://api.airtable.com/v0/appEHr8iHguvEfXTQ')
+    .persist()
+    .log(data => console.log(data))
+    .get('/Organizations?view=Grid+view')
+    .reply(200, {
+      records,
+    });
+
+  const organizations: Organization[] = await airtableApiClient.getAllOrganizations();
+  const answer: Organization[] = [org1, org2, org3];
+  t.deepEqual(organizations, answer);
+});
+
+test('Returns one page of filtered river results properly', async t => {
+  const airtableApiClient: AirTableApiClient = new AirTableApiClient('keyzxy');
+
+  nock('https://api.airtable.com/v0/appEHr8iHguvEfXTQ')
+    .persist()
+    .log(data => console.log(data))
+    .get('/Organizations?view=Grid+view')
+    .reply(200, {
+      records,
+    });
+
+  const airTableFilters: AirTableFilters = {
+    riverSections: ['Middle'],
+  };
+
+  const organizations: Organization[] = await airtableApiClient.getAllOrganizations(airTableFilters);
+  const answer: Organization[] = [org2];
+  t.deepEqual(organizations, answer);
+});
+
+test('Returns one page of filtered interest categories results properly', async t => {
+  const airtableApiClient: AirTableApiClient = new AirTableApiClient('keyzxy');
+
+  nock('https://api.airtable.com/v0/appEHr8iHguvEfXTQ')
+    .persist()
+    .log(data => console.log(data))
+    .get('/Organizations?view=Grid+view')
+    .reply(200, {
+      records,
+    });
+
+  const airTableFilters: AirTableFilters = {
+    interestCategories: ['Environmental Causes'],
+  };
+
+  const organizations: Organization[] = await airtableApiClient.getAllOrganizations(airTableFilters);
+  const answer: Organization[] = [org2];
+  t.deepEqual(organizations, answer);
+});
+
+test('Returns one page of filtered both empty results properly', async t => {
+  const airtableApiClient: AirTableApiClient = new AirTableApiClient('keyzxy');
+
+  nock('https://api.airtable.com/v0/appEHr8iHguvEfXTQ')
+    .persist()
+    .log(data => console.log(data))
+    .get('/Organizations?view=Grid+view')
+    .reply(200, {
+      records,
+    });
+
+  const airTableFilters: AirTableFilters = {
+    riverSections: ['Middle'],
+    interestCategories: ['Social Justice and Recreation'],
+  };
+
+  const organizations: Organization[] = await airtableApiClient.getAllOrganizations(airTableFilters);
+  const answer: Organization[] = [];
+  t.deepEqual(organizations, answer);
+});
+
+test('Returns one page of filtered both results properly', async t => {
+  const airtableApiClient: AirTableApiClient = new AirTableApiClient('keyzxy');
+
+  nock('https://api.airtable.com/v0/appEHr8iHguvEfXTQ')
+    .persist()
+    .log(data => console.log(data))
+    .get('/Organizations?view=Grid+view')
+    .reply(200, {
+      records,
+    });
+
+  const airTableFilters: AirTableFilters = {
+    riverSections: ['Middle'],
+    interestCategories: ['Social Justice and Recreation', 'Environmental Causes'],
+  };
+
+  const organizations: Organization[] = await airtableApiClient.getAllOrganizations(airTableFilters);
+  const answer: Organization[] = [org2];
+  t.deepEqual(organizations, answer);
+});
+
+test('Returns one page of filtered two interest categories results properly', async t => {
+  const airtableApiClient: AirTableApiClient = new AirTableApiClient('keyzxy');
+
+  nock('https://api.airtable.com/v0/appEHr8iHguvEfXTQ')
+    .persist()
+    .log(data => console.log(data))
+    .get('/Organizations?view=Grid+view')
+    .reply(200, {
+      records,
+    });
+
+  const airTableFilters: AirTableFilters = {
+    interestCategories: ['Social Justice and Recreation', 'Environmental Causes'],
+  };
+
+  const organizations: Organization[] = await airtableApiClient.getAllOrganizations(airTableFilters);
+  const answer: Organization[] = [org2, org3];
+  t.deepEqual(organizations, answer);
+});
+
+test('Returns one page of filtered two river results properly', async t => {
+  const airtableApiClient: AirTableApiClient = new AirTableApiClient('keyzxy');
+
+  nock('https://api.airtable.com/v0/appEHr8iHguvEfXTQ')
+    .persist()
+    .log(data => console.log(data))
+    .get('/Organizations?view=Grid+view')
+    .reply(200, {
+      records,
+    });
+
+  const airTableFilters: AirTableFilters = {
+    riverSections: ['Middle', 'Lower'],
+  };
+
+  const organizations: Organization[] = await airtableApiClient.getAllOrganizations(airTableFilters);
+  const answer: Organization[] = [org2, org3];
+  t.deepEqual(organizations, answer);
+});
