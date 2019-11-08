3991d969cc1461b494bbdbf586856d00f44df5df
Quinn Ngo <quinn.ngo@joinhoney.com>
2019-11-07 22:46:46 -0800

    Remove unnecessary middleware

--- src/routers/webhooks.ts
+++ src/routers/webhooks.ts
@@ -1,15 +1,12 @@
+import express from 'express';
+
 interface NationBuilderPerson {
   email: string;
   firstName: string;
   phone: string;
 }
 
-import express from 'express';
 const router = express.Router();
-
-router.use('/', (_, res, next) => {
-  next();
-});
 
 router.post('/nationbuilder/personCreated', function(req, res) {
   const { email, first_name: firstName, phone } = req.body.payload.person;
