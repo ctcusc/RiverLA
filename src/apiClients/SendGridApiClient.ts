f4e305c11920238a435d1e6d3668bf47270a2c86
Anthony Wiencko <wiencko@usc.edu>
2019-11-12 13:06:11 -0800

    Removed some console and testing lines

--- src/apiClients/SendGridApiClient.ts
+++ src/apiClients/SendGridApiClient.ts
@@ -11,12 +11,9 @@ class SendGridApiClient {
     }
     this.apiKey = apiKey;
     // remove this log when this API key is finally used somewhere
-    console.log(`SendGridAPIClient initialized with api key ${this.apiKey}`);
   }
 
   async sendEmail(senderEmailAddress: string, recipientEmailAddress: string, emailBody: string, emailSubject: string) {
-    console.log(`sendEmail called with arguments:`, senderEmailAddress, recipientEmailAddress, emailBody, emailSubject);
-
     sgMail.setApiKey(this.apiKey);
     const mailData: MailData = {
       to: recipientEmailAddress,
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import apiClients from './apiClients';
 import app from './app';
 import env from './env';
 
-app.listen(env.server.port, async () => {
+app.listen(env.server.port, () => {
   console.log(apiClients.airtableApiClient);
   console.log(apiClients.sendgridApiClient);
   console.log(`🚀 App listening on port ${env.server.port}!`);
