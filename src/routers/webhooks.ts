9ee0e147d40e2618d3595f7fa31693adf7635258
Peidi Xie <cindyxie@usc.edu>
2019-11-13 18:30:39 -0800

    Continuous deployment with Heroku through CircleCI (#17)

--- src/routers/webhooks.ts
+++ src/routers/webhooks.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.use('/', (_, res) => {
-  res.send('webhook route');
+  res.send('webhook routers');
 });
 
 export default router;
