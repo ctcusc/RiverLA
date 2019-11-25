5939bb1ec202154d6d13fc759003f7707dc87aa9
Madelyn Dubuk <madelyndubuk@Madelyns-MacBook-Pro.local>
2019-11-25 12:58:31 -0800

    added filtering for webhook

--- src/routers/webhooks.ts
+++ src/routers/webhooks.ts
@@ -8,14 +8,17 @@ interface NationBuilderPerson {
 
 const router = express.Router();
 
-router.post('/nationbuilder/personCreated', function(req, res) {
-  const { email, first_name: firstName, phone } = req.body.payload.person;
-  const nationBuilderPerson: NationBuilderPerson = {
-    email,
-    firstName,
-    phone,
-  };
-  res.json(nationBuilderPerson);
+router.post('/nationbuilder/personCreated', function(req: any, res: any) {
+  if (req.body.payload.person.is_volunteer) {
+    const { email, first_name: firstName, phone } = req.body.payload.person;
+    const nationBuilderPerson: NationBuilderPerson = {
+      email,
+      firstName,
+      phone,
+    };
+    console.log(nationBuilderPerson);
+    res.json(nationBuilderPerson);
+  }
 });
 
 export default router;
