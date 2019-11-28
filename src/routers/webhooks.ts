df10a601063d15b5177b16c945e5fb16275ac731
Daniel Koo <dkoo.dev@gmail.com>
2019-11-27 16:36:34 -0800

    Can retrieve data from volunteer form
    Need to work on getting organization data and sending email

--- src/routers/webhooks.ts
+++ src/routers/webhooks.ts
@@ -1,21 +1,52 @@
+import apiClients from '../apiClients';
 import express from 'express';
+const { airtableApiClient, sendgridApiClient } = apiClients;
 
 interface NationBuilderPerson {
   email: string;
   firstName: string;
   phone: string;
+  interests: {
+    water: boolean;
+    environmental: boolean;
+    people: boolean;
+  };
 }
 
 const router = express.Router();
 
 router.post('/nationbuilder/personCreated', function(req, res) {
-  const { email, first_name: firstName, phone } = req.body.payload.person;
+  const { email, first_name: firstName, phone, tags } = req.body.payload.person;
+  let interests = {
+    water: true,
+    environmental: true,
+    people: true,
+  };
+  if (tags.includes('Action: Volunteer Yes: All activities')) {
+    interests = {
+      water: true,
+      environmental: true,
+      people: true,
+    };
+  } else {
+    interests = {
+      water: tags.includes('Action: Volunteer Yes: Water Organizations'),
+      environmental: tags.includes('Action: Volunteer Yes: Environmental'),
+      people: tags.includes('Action: Volunteer Yes: People Organizations'),
+    };
+  }
+
   const nationBuilderPerson: NationBuilderPerson = {
     email,
     firstName,
     phone,
+    interests,
   };
-  res.json(nationBuilderPerson);
+
+  console.log(sendgridApiClient, airtableApiClient); // adding to get rid of error
+  //TODO: use airtableapi to bring in organization data and use it to send an email to the client.
+
+  res.json(nationBuilderPerson); // TODO: don't need this anymore?
 });
 
 export default router;
