75d7d4cc577b981318a846e1bea97cc5319a9722
Peidi Xie <cindyxie@usc.edu>
2020-02-19 01:01:55 -0800

    add documentation to cacheditem class

--- src/utils/CachedItem.ts
+++ src/utils/CachedItem.ts
@@ -1,3 +1,10 @@
+/**
+ * A generic cache class to cache AirTable organizations
+ *
+ * This class acts as an in-memory caching layer for requests to AirTable server,
+ * which stores recently retrieved AirTable organizations to help the users adhere
+ * to the rate limit of AirTable API.
+ */
 class CachedItem<T> {
   private cache: T | null;
   private ttl: number;
@@ -8,6 +15,13 @@ class CachedItem<T> {
     this.ttl = ttl;
   }
 
+  /**
+   * Retrieves an item from the cache or returns null if no item is found.
+   * Uses the Date object to identify if the item has reached its expiration time.
+   * If the item has been stored for `ttl` milliseconds, invalidates the item.
+   *
+   * @returns the item of type T or null if the item is invalidated
+   */
   get(): T | null {
     if (this.startTime !== undefined) {
       if (new Date().getTime() - this.startTime.getTime() >= this.ttl) {
@@ -17,6 +31,13 @@ class CachedItem<T> {
     return this.cache;
   }
 
+  /**
+   * Adds an item to the cache.
+   * Uses the Date object to record the time the item is added, which helps identify
+   * if the item needs to be expired after `ttl` milliseconds in get() function.
+   *
+   * @param value - an item of type T to be added to the cache
+   */
   set(value: T): void {
     this.cache = value;
     this.startTime = new Date();
