16972d9d8c5161764b0eded0adeee144594434bc
Olivia Hong <oliviahong97@gmail.com>
2020-02-25 20:42:21 -0800

    Adding documentation for air table api client (#32)
    * adding documentation for airtableapiclient
    
    * syntax error
    
    * another typo
    
    * adding references and other small edits

--- src/apiClients/AirTableApiClient.ts
+++ src/apiClients/AirTableApiClient.ts
@@ -1,3 +1,10 @@
+/**
+ * Main client to interact with AirTable API.
+ *
+ * The [[AirTableApiClient]] class holds wrapper functions to call AirTable's API as well
+ * a function to log errors in AirTable.
+ */
+
 import Airtable from 'airtable';
 import CachedItem from '../utils/CachedItem';
 import env from '../env';
@@ -41,6 +48,12 @@ interface Record {
   };
 }
 
+/**
+ * Takes raw data from AirTable API call and stores it in a Record object.
+ *
+ * @param record - [[Record]] object containing one row's data (corresponding to one organization) from AirTable
+ * @returns A new Organization object containing data from the record parameter
+ */
 function recordToOrganization(record: Record): Organization {
   return {
     name: record.fields['Name'],
@@ -59,6 +72,13 @@ const BASE_NAMES = {
   ERRORS: 'Errors',
 };
 
+/**
+ * A wrapper class to interact with the AirTable API.
+ *
+ * This class acts as a client to make calls to AirTable's API for
+ * one specific table, which is idenitified by the base variable.
+ *
+ */
 class AirTableApiClient {
   private base: any;
   private cachedOrganizations: CachedItem<Organization[]>;
@@ -68,6 +88,19 @@ class AirTableApiClient {
     this.cachedOrganizations = new CachedItem<Organization[]>(1000);
   }
 
+  /**
+   * Returns a list of organizations by making an API call to AirTable.
+   * Uses the API's select method, which makes a GET request to the Organizations endpoint.
+   * Takes in optional filters to retrieve organizations with specific fields.
+   *
+   * @param filters - Object containing lists of options to filter by. If these lists are empty,
+   * no organizations will be filtered out. Within each list, organizations only need to satisfy at least one
+   * of the desired options. For example, if multiple interest categories are specified, an organization only
+   * needs to match one of them to be returned. Between the lists, an organization must satisfy at least one
+   * option from each. Therefore, an organization would need to have at least one of the desired interest
+   * categories AND one of the desired river sections to prevent being filtered out.
+   * @returns An array of [[Organization]] objects satisfying the desired filters
+   */
   async getOrganizations(filters: AirTableFilters = {}): Promise<Organization[]> {
     const { interestCategories, riverSections } = filters;
     let organizations = this.cachedOrganizations.get();
