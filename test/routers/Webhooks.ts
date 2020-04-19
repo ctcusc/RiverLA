b117fba5b8629b86b2dafa6608f3b1150781422f
Yash Chandak <ychandak@usc.edu>
2020-04-19 12:12:40 -0500

    Increase branch coverage of tags

--- test/routers/Webhooks.ts
+++ test/routers/Webhooks.ts
@@ -20,7 +20,11 @@ let person = {
   email: 'person1@gmail.com',
   first_name: 'person1',
   is_volunteer: true,
-  tags: ['Action: Volunteer Yes: People Organizations'],
+  tags: [
+    'Action: Volunteer Yes: Water Organizations',
+    'Action: Volunteer Yes: Environmental',
+    'Action: Volunteer Yes: People Organizations',
+  ],
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -48,7 +52,7 @@ person = {
   email: 'person2@gmail.com',
   first_name: 'person2',
   is_volunteer: false,
-  tags: ['Action: Volunteer Yes: Water Organizations', 'Action: Volunteer Yes: Environmental'],
+  tags: ['Action: Volunteer Yes: Water Organizations'],
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
